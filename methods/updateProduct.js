ba6710354c76a1b8bc59beb93f0239bdfac6e7c2
Joe Hill <joehill331@gmail.com>
2019-06-14 13:54:22 +0100

    WIP

--- methods/updateProduct.js
+++ methods/updateProduct.js
@@ -1,3 +1,17 @@
-const updateProduct = product => {};
+const Moltin = require('../client/moltin-client');
+
+const updateProduct = async products => {
+  for (let product in products) {
+    try {
+        const updatedProduct = await Moltin().put('products', {
+            id: product.id
+
+        })
+        console.log(`Product updated: ${updatedProduct.data.name}`)
+    } catch (error) {
+        console.error(error)
+    }
+  }
+};
 
 module.exports = updateProduct;
