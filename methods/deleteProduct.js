0e8b24b186e840e861cc162335eb8bb7ad795cc8
Joe Hill <joehill331@gmail.com>
2019-06-14 13:47:11 +0100

    Products now gets passed in

--- methods/deleteProduct.js
+++ methods/deleteProduct.js
@@ -1,21 +1,15 @@
 const Moltin = require('../client/moltin-client');
-const getProducts = require('../client/csv-client');
 
-const deleteProduct = async () => {
-  try {
-    const products = await getProducts();
-    products.forEach(async product => {
-      try {
-        await Moltin().delete('products', {
-          id: product.id
-        });
-      } catch (error) {
-        console.error(error);
-      }
-    });
-  } catch (error) {
-    console.error(error);
-  }
+const deleteProduct = async products => {
+  products.forEach(async product => {
+    try {
+      await Moltin().delete('products', {
+        id: product.id
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  });
 };
 
 module.exports = deleteProduct;
